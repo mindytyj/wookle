6cd148cfbe14273e510c3d401ebbf7e9c34f8aa5
mindytyj <derpbritz@gmail.com>
2023-06-07 23:48:27 +0800

    Add API for Testing

--- wookle/src/App.jsx
+++ wookle/src/App.jsx
@@ -1,9 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
 function App() {
-  return <></>;
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    async function displayAPI() {
+      const response = await fetch("https://swapi.dev/api/people");
+      const jsonData = await response.json();
+      setCharacters(jsonData.results);
+    }
+    displayAPI();
+  }, []);
+
+  return (
+    <ul>
+      {characters.map((character, index) => (
+        <li key={index}>{character.name}</li>
+      ))}
+    </ul>
+  );
 }
 
 export default App;
